33b02a7eebc17d541b5a2f608a0247aa9201612f
prashitprakhar <32621695+prashitprakhar@users.noreply.github.com>
2019-10-17 11:56:28 -0500

    Update custom-order-modal.component.ts

--- src/app/modals/custom-order-modal/custom-order-modal.component.ts
+++ src/app/modals/custom-order-modal/custom-order-modal.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+export interface ICustomItem {
+  itemId: string;
+  itemName: string;
+  itemQuantity: number;
+  itemCount: number;
+}
 
 @Component({
   selector: 'app-custom-order-modal',
@@ -7,8 +15,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomOrderModalComponent implements OnInit {
 
-  constructor() { }
+  public customItemsCount: number = 0;
+  public customItemsArray: ICustomItem[] = [
+    {
+      itemId: 'item1',
+      itemName: '',
+      itemQuantity: 0,
+      itemCount: 0
+    }
+  ];
+
+  constructor(private modalCtrl: ModalController) { }
 
   ngOnInit() {}
+
+  onClose() {
+    this.modalCtrl.dismiss(null, 'cancel', 'customItemModal')
+  }
+
+  addNewItem() {
+    ++this.customItemsCount;
+
+  }
+
+  increment(itemId){
+    console.log("Item ID increment")
+    let customItem = this.customItemsArray.find(item => item.itemId === itemId);
+    customItem.itemCount++;
+    console.log("Item Count***",customItem.itemCount);
+  }
+
+  onComplete() {
+    this.modalCtrl.dismiss({message : 'Passing data back from modal'}, 'confirm', 'customItemModal')
+  }
 
 }
